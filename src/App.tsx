50c1f8697c5d5fbbec16804101b4c33960871f35
yiwei <willyos0113@gmail.com>
2025-07-26 18:17:24 +0800

    表單範例: 使用 BULMA 預設表單，調整App.tsx 註釋，供後續開發使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,119 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "bulma/css/bulma.min.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="container"
+        style={{ maxWidth: "600px", margin: "2rem auto", padding: "2rem" }}
+      >
+        <h1 className="title">Bulma form example</h1>
+
+        <div>
+          {/* input button for entering your name */}
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Text input" />
+            </div>
+          </div>
+
+          {/* input button for entering your username */}
+          <div className="field">
+            <label className="label">Username</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input is-success"
+                type="text"
+                placeholder="Text input"
+                defaultValue="bulma"
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-user"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </div>
+            <p className="help is-success">This username is available</p>
+          </div>
+
+          {/* input button for entering your Email */}
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input is-danger"
+                type="email"
+                placeholder="Email input"
+                defaultValue="hello@"
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+            <p className="help is-danger">This email is invalid</p>
+          </div>
+
+          {/* select-options for indicating a subject */}
+          <div className="field">
+            <label className="label">Subject</label>
+            <div className="control">
+              <div className="select">
+                <select>
+                  <option>Select dropdown</option>
+                  <option>With options</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          {/* Textarea for message */}
+          <div className="field">
+            <label className="label">Message</label>
+            <div className="control">
+              <textarea className="textarea" placeholder="Textarea"></textarea>
+            </div>
+          </div>
+
+          {/* Checkbox for agreements */}
+          <div className="field">
+            <div className="control">
+              <label className="checkbox">
+                <input type="checkbox" />I agree to the{" "}
+                <a href="#">terms and conditions</a>
+              </label>
+            </div>
+          </div>
+
+          {/* yes or no radio buttons */}
+          <div className="field">
+            <div className="control">
+              <label className="radio">
+                <input type="radio" name="question" />
+                Yes
+              </label>{" "}
+              <label className="radio">
+                <input type="radio" name="question" />
+                No
+              </label>
+            </div>
+          </div>
+
+          {/* submit and cancel buttons */}
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+            <div className="control">
+              <button className="button is-link is-light">Cancel</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
-}
-
-export default App;
+};
